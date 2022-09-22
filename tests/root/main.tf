8cf26dbee685e3e011448d931696d1e0e94f4d84
Joerg Kritzen <j.kritzen@ontracon.de>
2022-09-22 17:32:13 +0200

    Test

--- tests/root/main.tf
+++ tests/root/main.tf
@@ -1,3 +1,26 @@
 resource "random_pet" "name" {
-  length = 2
+  length = 5
+}
+
+# RSA key of size 4096 bits
+resource "tls_private_key" "rsa-4096-example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "tls_self_signed_cert" "example" {
+  private_key_pem = resource.tls_private_key.rsa-4096-example.private_key_pem
+
+  subject {
+    common_name  = "example.com"
+    organization = "ACME Examples, Inc"
+  }
+
+  validity_period_hours = 12
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
 }
--- tests/root/outputs.tf
+++ tests/root/outputs.tf
@@ -1,3 +1,7 @@
 output "pet_name" {
   value = random_pet.name
 }
+
+output "cert_pem" {
+  value = resource.tls_self_signed_cert.example.cert_pem
+}
