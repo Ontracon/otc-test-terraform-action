716f66ca9f8c13662138b7cffa3328b05e108bf7
Joerg Kritzen <j.kritzen@ontracon.de>
2022-09-26 16:46:37 +0200

    test aws

--- tests/aws/versions.tf
+++ tests/aws/versions.tf
@@ -5,9 +5,9 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3"
     }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.2.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.11"
     }
   }
 }
